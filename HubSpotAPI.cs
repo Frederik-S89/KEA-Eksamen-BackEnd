86990fb8baedc98842fea8ca56440e5e915c6a09
Frederik-S89 <71264643+Frederik-S89@users.noreply.github.com>
2023-01-13 06:39:54 +0100

    Add files via upload

---
+++ HubSpotAPI.cs
@@ -0,0 +1,10 @@
+﻿namespace HubSpotJul
+{
+    public class HubSpotAPI
+    {
+        public int Id { get; set; }        
+        public string Name { get; set; } = String.Empty;
+        public string Password { get; set; } = String.Empty;
+
+    }
+}
